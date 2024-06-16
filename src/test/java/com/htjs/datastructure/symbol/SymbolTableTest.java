40885f3a9430e43ba60b33248e9cc3c4ce5f08d6
tianyu <tianyu.hpu@gmail.com>
2024-06-16 14:18:04 +0800

    符号表

---
+++ src/test/java/com/htjs/datastructure/symbol/SymbolTableTest.java
@@ -0,0 +1,19 @@
+package com.htjs.datastructure.symbol;
+
+public class SymbolTableTest {
+
+    public static void main(String[] args) {
+        SymbolTable<Integer, String> st = new SymbolTable<>();
+        st.put(1, "张三");
+        st.put(3, "李四");
+        st.put(5, "王五");
+        System.out.println(st.size());
+        st.put(1,"老三");
+        System.out.println(st.get(1));
+        System.out.println(st.size());
+        st.delete(1);
+        System.out.println(st.size());
+
+    }
+
+}
