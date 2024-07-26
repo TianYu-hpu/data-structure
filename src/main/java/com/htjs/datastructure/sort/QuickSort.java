1c10639088420e652f6a02c45d7f3631eaf341dc
tianyu <tianyu.hpu@gmail.com>
2024-07-26 20:52:58 +0800

    豆包 Java 快速排序算法

--- src/main/java/com/htjs/datastructure/sort/QuickSort.java
+++ src/main/java/com/htjs/datastructure/sort/QuickSort.java
@@ -5,46 +5,35 @@ import java.util.Arrays;
 public class QuickSort {
 
 
-    public static void quickSort(Comparable[] arr,int low,int high){
-        //基准元素
-        Comparable pivot = arr[(low + high) / 2];
-        int left = low;
-        int right = high;
-        while(left < right) {
-            while(less(arr[left], pivot)) {
-                ++left;
-            }
-
-            while(less(pivot, arr[right])) {
-                --right;
-            }
-            if(left <= right) {
-                exchange(arr, left ,right);
-                --right;
-                ++left;
-                System.out.println(Arrays.toString(arr));
-            }
-        }
-        if(left == right) {
-            left++;
-        }
-        if(low < right) {
-            quickSort(arr, low, left - 1);
-        }
-        if(left < high) {
-            quickSort(arr, right + 1, high);
-        }
-
+    // 交换数组中的两个元素
+    public static void swap(Integer[] arr, int i, int j) {
+        Integer temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
-    private static void exchange(Comparable[] a, int i, int j) {
-        Comparable temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+    // 划分函数，选择一个基准元素，将小于基准的元素放在左边，大于基准的元素放在右边
+    public static int partition(Integer[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
     }
 
+    // 快速排序的主函数
+    public static void quickSort(Integer[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
 
-    private static boolean less(Comparable comparable, Comparable comparable1) {
-        return comparable.compareTo(comparable1) < 0;
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
     }
 }
